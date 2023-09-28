70cdb6768418712fcc2a1756f8b634de6e52bad3
nezirnezirevic310 <1054620@hr.nl>
2023-09-28 22:16:13 +0200

    refactor button

---
+++ classes/button.py
@@ -0,0 +1,8 @@
+class Butoon:
+    def __init__(self, image, x_position, y_position):
+        self.image = image
+        self.x_position = x_position
+        self.y_position = y_position
+
+    def draw():
+        pass
