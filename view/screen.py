9b4a3a152b877bd2381b3064f8a7cd3360060b2d
Laura1062463 <1062463@hr.nl>
2023-10-04 09:55:53 +0200

    gameover_fl_img - intergrated in order to move on top of gameover_bg_img

--- view/screen.py
+++ view/screen.py
@@ -58,6 +58,15 @@ def game_over_screen(canvas, font, text_color, SCREEN_WIDTH, SCREEN_HEIGHT):
         center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 50)
     )
 
+    # initial positie van gameover_fl_img
+    fl_x = 0
+    fl_y = 0
+    
+    # snelheid
+    fl_speed_x = 2
+    fl_speed_y = 1
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -70,4 +79,18 @@ def game_over_screen(canvas, font, text_color, SCREEN_WIDTH, SCREEN_HEIGHT):
 
         canvas.blit(game_over_text, game_over_rect)
         canvas.blit(restart_text, restart_rect)
+        pygame.display.flip()
+
+        # beweeg gameover_fl_img
+        fl_x += fl_speed_x
+        fl_y += fl_speed_y
+
+        # als gameover_fl_img buiten het scherm gaat, keert deze terug
+        if fl_x >= SCREEN_WIDTH:
+            fl_x = 0
+        
+        if fl_y >= SCREEN_HEIGHT:
+            fl_y = 0
+
+        canvas.blit(gameover_fl_img, (fl_x, fl_y))
         pygame.display.flip()
