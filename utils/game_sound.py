133b9425acd720bcc118fc91c42dd4875a6cf106
nezirnezirevic310 <1054620@hr.nl>
2023-10-14 20:01:49 +0200

    lowered background and game over music

--- utils/game_sound.py
+++ utils/game_sound.py
@@ -7,6 +7,7 @@ pygame.mixer.init()
 def background_music():
     background_sound_path = "sounds/bg_sound.mp3"
     mixer.music.load(background_sound_path)
+    mixer.music.set_volume(0.2)
     mixer.music.play(-1)
 
 
@@ -19,4 +20,5 @@ def press_button_sound():
 def game_over_sound():
     game_over_sound_path = "sounds/game_over_sound.mp3"
     mixer.music.load(game_over_sound_path)
+    mixer.music.set_volume(0.2)
     mixer.music.play(0)
