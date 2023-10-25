139d8158291d4b5e1474f1a84813b03facfa23ba
Laura1062463 <1062463@hr.nl>
2023-10-25 11:39:28 +0200

    bug fix: enemies now disappear when getting shot

--- screens/start_game_screen.py
+++ screens/start_game_screen.py
@@ -137,6 +137,16 @@ def start_game_screen(canvas, font, SCREEN_WIDTH, GAME_SPEED, SCREEN_HEIGHT, tex
                 player.remove(all_sprites)
         enemies = updated_enemies
 
+        for bullet in bullets:
+            # Check for collision
+            for enemy_x, enemy_y in enemies:
+                if bullet.rect.colliderect(pygame.Rect(enemy_x, enemy_y, ENEMY_WIDTH, ENEMY_HEIGHT)):
+                    # Handle collision
+                    enemies.remove([enemy_x, enemy_y])
+                    score += 100
+                    score_counter = font.render(
+                        f'Score: {score // 60}', True, (255, 255, 255))
+
         all_sprites.draw(canvas)
         pygame.display.flip()
         for event in pygame.event.get():
