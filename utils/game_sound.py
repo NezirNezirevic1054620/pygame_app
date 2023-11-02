94956e9c2bc5a67948fb20c8145c998fc69f84d6
nezirnezirevic310 <1054620@hr.nl>
2023-11-02 20:18:04 +0100

    fixed sounds

--- utils/game_sound.py
+++ utils/game_sound.py
@@ -7,7 +7,7 @@ pygame.mixer.init()
 def background_music():
     background_sound_path = "sounds/bg_sound.mp3"
     mixer.music.load(background_sound_path)
-    mixer.music.set_volume(0.01)
+    mixer.music.set_volume(0.2)
     mixer.music.play(-1)
 
 
@@ -27,10 +27,12 @@ def game_over_sound():
 def collision_sound():
     collision_sound_path = "sounds/block_hit.mp3"
     collision_sound = pygame.mixer.Sound(collision_sound_path)
+    mixer.music.set_volume(0.2)
     collision_sound.play(0)
 
 
 def dead_sound():
     dead_sound_path = "sounds/space_shooter_dead.mp3"
     dead_sound = pygame.mixer.Sound(dead_sound_path)
+    mixer.music.set_volume(0.2)
     dead_sound.play(0)
